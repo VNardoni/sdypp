fda7d6e56ee595ff70d4afb29c9c9f2ed98f0b0f
Valentin Nardoni <vnardoni@live.com.ar>
2024-03-11 21:41:11 -0300

    Prueba VM

---
+++ demo-gcloud/script.sh
@@ -0,0 +1,18 @@
+# Update package lists
+sudo apt update
+
+# Install Node.js 18.x
+sudo apt install -y nodejs
+
+
+# Install OpenJDK
+sudo apt install -y openjdk-18-jdk
+sudo apt install -y mvn
+java --version
+nodejs --version
+
+sudo apt install nginx -y
+sudo systemctl enable
+sudo systemctl start nginx
+
+sudo systemctl status nginx
